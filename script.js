63a41ba49d6af8e09c8094ba704c45f305cb555c
aliissonreiis <aliisson.reiis13@gmail.com>
2023-03-01 11:29:15 -0300

    addarqjs

---
+++ script.js
@@ -0,0 +1,86 @@
+const repositories = document.querySelector('.repositorios');
+const user_perfil = document.querySelector('.grupo-infos')
+
+
+function s(){
+    
+   var user = document.getElementById('input-search').value;
+   console.log(user);
+   limpRepos();
+   getReposGitHub(user);
+    
+  
+
+}
+
+function getReposGitHub(user){
+    
+    fetch('https://api.github.com/users/'+user+'/repos')
+    .then(async res => {
+        if(!res.ok){
+           // throw new Error(res.status);
+            alert("Usuário não encontrado");
+            location.reload();
+        }
+        let data = await res.json();
+        data.map( item =>{
+        let project = document.createElement('div');
+        project.innerHTML = `
+        <div class = "project">
+             <div>
+                <h4 class ="title"><a href="${item.html_url}">${item.name}</a></h4>
+    
+            </div>
+        
+        </div>
+        `
+    
+        
+        repositories.appendChild(project);
+        
+        })
+    fetch('https://api.github.com/users/'+user)
+        .then(async resx =>{
+            if(!resx.ok){
+                throw new Error(resx.status);
+            }
+            let data = await resx.json();
+                [data].map(itens => {
+                let arr = [data];
+                let a = console.log(arr.length);
+                    if (arr === 0){
+                        console.log("Usuario não encontrado");
+                    }
+                let _name = document.querySelector('.textfullname');
+                let user_name = document.querySelector('.username-rep');
+                let location = document.querySelector('.text_localization');
+                let bio = document.querySelector('.text_bio');
+                let _user_name = document.querySelector('.user-name');
+                document.getElementById('img-perfil').src = itens.avatar_url;
+                _name.textContent = itens.name;
+                user_name.textContent = itens.login
+                location.textContent = itens.location;
+                bio.textContent = itens.bio;
+                _user_name.textContent = itens.login;
+                
+                
+                
+            })
+            
+        })
+        
+    })
+   
+}
+
+function limpRepos(){
+    
+    var lista = document.getElementsByClassName("project");
+    console.log(lista)
+    if(lista != null){
+    for(var i = lista.length - 1; i >= 0; i--)
+    {
+        lista[i].remove()
+    }
+
+}}
